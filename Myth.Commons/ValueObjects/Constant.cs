47f2e209c2db0dee613aad69cf2d5ba706f118bc
Paulo Leal <pauloolileal@gmail.com>
2021-04-03 12:54:41 -0300

    - Alterado constante

--- Myth.Commons/ValueObjects/Constant.cs
+++ Myth.Commons/ValueObjects/Constant.cs
@@ -1,37 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Ardalis.SmartEnum;
 
 namespace Myth.ValueObjects {
 
-    public class Constant<T> : IEquatable<Constant<T>> {
+    public abstract class Constant<T> : SmartEnum<Constant<T>> {
+        private static int _value;
+        public Constant( string name, int value ) : base( name, value ) { }
 
-        public T Value { get; private set; }
+        public Constant( string name ) : this( name, ++_value ) { }
 
-        protected Constant( T value ) {
-            Value = value;
-        }
-
-        public static implicit operator T( Constant<T> a ) =>
-             a.Value;
-
-        public static implicit operator Constant<T>( T a ) =>
-             new Constant<T>( a );
-
-        public static bool operator !=( Constant<T> a, Constant<T> b ) =>
-             a.ToString( ) != b.ToString( );
-
-        public static bool operator ==( Constant<T> a, Constant<T> b ) =>
-             a.ToString( ) == b.ToString( );
-
-        public override string ToString( ) => Value.ToString( );
-
-        public virtual bool Equals( Constant<T> other ) =>
-             other.Value.ToString( ) == Value.ToString( );
-
-        public override int GetHashCode( ) =>
-             -1937169414 + EqualityComparer<T>.Default.GetHashCode( Value );
-
-        public override bool Equals( object obj ) =>
-             base.Equals( obj );
+        public static implicit operator string( Constant<T> constant ) => constant.Name;
+        public static implicit operator int( Constant<T> constant ) => constant.Value;
     }
 }
