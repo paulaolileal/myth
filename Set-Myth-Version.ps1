87dc411421be44b225a1c3696cd9207988be85cb
Paulo Victor de Oliveira Leal <pauloolileal@gmail.com>
2022-02-17 10:29:41 -0300

    - Atualizado versão dos pacotes Myths

--- Set-Myth-Version.ps1
+++ Set-Myth-Version.ps1
@@ -3,7 +3,7 @@ if($args[0] -ne $null -And $args[0] -ne ''){
 	$version = $args[0]
 	$current = $PSScriptRoot # Get the current script path
 
-	# Replace all the versions of Harpy reference to the new one
+	# Replace all the versions of Myth reference to the new one
 	$files = Get-ChildItem -Path $current -Include *.csproj -Recurse | % { $_.FullName }	
 	foreach($file in $files) {
 		$content = (Get-Content -Path $file -Raw)		
@@ -20,23 +20,34 @@ if($args[0] -ne $null -And $args[0] -ne ''){
 		
 		Set-Content -Path $file -Value ($content.Trim())
 	}
+	Write-Host "Myth packages set to $($version) in all procjects" -fore green
 	
-	# Remove all harpy packages generated
+	# Remove all myth packages generated
 	$packages =  Get-ChildItem -Filter "myth*" -Path $ENV:UserProfile\AppData\Roaming\NuGet\Packages
 	foreach($pack in $packages){
 		Remove-Item $pack.FullName -Recurse
 	}
+	Write-Host "Myth packages cleaned from local Nuget Packages" -fore green
 
-	# Remove all harpy packages cached
+	# Remove all myth packages cached
 	$harpyFolders = Get-ChildItem -Directory -Filter "myth*" -Path $ENV:UserProfile\.nuget\packages
 	foreach($folder in $harpyFolders){
 		Remove-Item $folder.FullName -Recurse
 	}
+	Write-Host "Myth packages cleaned from cached Nuget Packages" -fore green
 	
 	$projects = @(	
-		"$($current)\Myth.Commons\Myth.csproj")
+		"$($current)\Myth.Commons\Myth.Commons.csproj",
+		"$($current)\Myth.Specification\Myth.Specification.csproj",		
+		"$($current)\Myth.Repository\Myth.Repository.csproj",		
+		"$($current)\Myth.Repository.EntityFramework\Myth.Repository.EntityFramework.csproj",		
+		"$($current)\Myth.Rest\Myth.Rest.csproj",		
+		"$($current)\Myth.Odata\Myth.Odata.csproj"	
+		)
 	
 	foreach($file in $projects) {
+		Write-Host "Starting build package $($file)..." -fore blue
+		
 		if($version -Like '*stage*'){
 			dotnet build $file -v quiet -c Debug 
 			dotnet pack $file -o $ENV:UserProfile\AppData\Roaming\NuGet\Packages -v quiet -c Debug
