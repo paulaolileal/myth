650262966bc46f30c370bddbfd0980976396aa7e
Paulo Victor de Oliveira Leal <pauloolileal@gmail.com>
2022-04-30 19:39:04 -0300

    - Implementado rest factory

---
+++ Myth.Rest/DependencyInjection/RestFactory.cs
@@ -0,0 +1,16 @@
+﻿using Myth.Rest;
+
+namespace Myth.DependencyInjection {
+
+    public class RestFactory {
+        private IDictionary<string, RestBuilder> _clients;
+
+        public RestFactory( ) {
+            _clients = new Dictionary<string, RestBuilder>( );
+        }
+
+        public void Add( string name, RestBuilder builder ) => _clients.TryAdd( name, builder );
+
+        public RestBuilder Get( string name ) => _clients[ name ];
+    }
+}
