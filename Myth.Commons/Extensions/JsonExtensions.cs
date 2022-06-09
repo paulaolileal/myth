518ee7d628ea8d3ec24f1b28308fc62c07eba218
Paulo Victor de Oliveira Leal <pauloolileal@gmail.com>
2022-06-09 12:18:30 -0300

    - Implementado metodo para deserialização

--- Myth.Commons/Extensions/JsonExtensions.cs
+++ Myth.Commons/Extensions/JsonExtensions.cs
@@ -30,6 +30,32 @@ namespace Myth.Extensions {
             }
         }
 
+        public static object? FromJson( this string content, Type responseType, CaseStrategy caseStrategy = CaseStrategy.CamelCase, bool ignoreNullValue = true, Action<JsonSerializerSettings>? settings = null ) {
+            var contractResolver = new DefaultContractResolver {
+                NamingStrategy = StrategyResolver( caseStrategy )
+            };
+
+            var jsonSettings = new JsonSerializerSettings {
+                Formatting = Formatting.Indented,
+                NullValueHandling = ignoreNullValue ? NullValueHandling.Ignore : NullValueHandling.Include,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                ContractResolver = contractResolver
+            };
+
+            if ( settings is not null )
+                settings.Invoke( jsonSettings );
+
+            try {
+                return JsonConvert.DeserializeObject( content, responseType, jsonSettings );
+            } catch ( Exception exception ) {
+                throw new JsonException( "Error on deserialize object.", exception );
+            }
+        }
+
+        public static TResponse? FromJson<TResponse>( this string content, CaseStrategy caseStrategy = CaseStrategy.CamelCase, bool ignoreNullValue = true, Action<JsonSerializerSettings>? settings = null ) {
+            return (TResponse?) content.FromJson( typeof( TResponse ), caseStrategy, ignoreNullValue, settings );
+        }
+
         public static HttpContent ToHttpContent( this object content, CaseStrategy caseStrategy = CaseStrategy.CamelCase ) {
             return new StringContent( content.ToJson( caseStrategy: caseStrategy ), Encoding.UTF8, "application/json" );
         }
