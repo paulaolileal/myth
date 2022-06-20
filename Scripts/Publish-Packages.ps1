d14a098e323e1ff1004d974188f6c85cff5c9810
Paulo Victor de Oliveira Leal <pauloolileal@gmail.com>
2022-06-20 16:55:21 -0300

    - Alterado para versão 2.0.0.6

--- Scripts/Publish-Packages.ps1
+++ Scripts/Publish-Packages.ps1
@@ -1,6 +1,6 @@
 $path = "$ENV:UserProfile\AppData\Roaming\NuGet\Packages"
 $files = Get-ChildItem -Path $path -Name -Exclude *dev* -Include *.nupkg -ErrorAction SilentlyContinue | Sort-Object
-$key = 'oy2j4fxvuo7bjhqj5ngqe3rjy62jhqzuvjlws4f4gvtdyi'
+$key = 'oy2kc47sswxqmf63zwvqbflfvntrba5ubixvopdndyxwni'
 foreach($file in $files){
 	$package = "$($path)\$($file)"
 	Write-Host "Publicando pacote: $($file)" -fore blue
