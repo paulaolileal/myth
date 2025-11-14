22945a0cb8a440bcc63296512ecbaa05e21065d4
Paula Victoria de Oliveira Leal <paulaolileal@gmail.com>
2025-11-14 19:30:54 -0300

    feat(guard): updated validation models to public

--- Myth.Guard/Models/ErrorDetail.cs
+++ Myth.Guard/Models/ErrorDetail.cs
@@ -3,7 +3,7 @@ namespace Myth.Models;
 /// <summary>
 /// Error detail model
 /// </summary>
-internal sealed class ErrorDetail {
+public sealed class ErrorDetail {
 	public string Field { get; set; } = string.Empty;
 	public string Message { get; set; } = string.Empty;
 	public string Code { get; set; } = string.Empty;
--- Myth.Guard/Models/ValidationErrorResponse.cs
+++ Myth.Guard/Models/ValidationErrorResponse.cs
@@ -3,7 +3,7 @@ namespace Myth.Models;
 /// <summary>
 /// Validation error response model
 /// </summary>
-internal sealed class ValidationErrorResponse {
+public sealed class ValidationErrorResponse {
 	public string Code { get; set; } = string.Empty;
 	public List<ErrorDetail> Errors { get; set; } = [ ];
 }
